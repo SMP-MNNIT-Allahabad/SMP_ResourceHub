0af3965de4b1c4df58f0065d139104b112cdeca2
Ujjwal Gupta <143928527+ujjwalguptaMNNIT@users.noreply.github.com>
2024-12-10 22:43:50 +0530

    Update creator_js.js

--- js/creator_js.js
+++ js/creator_js.js
@@ -23,6 +23,23 @@ const contributorsData = [
     linkedin: "https://www.linkedin.com/in/shradhey-pathak",
     image: "./images/creators/Shradhey Pathak.jpg",
   },
+
+ {
+
+name: "Ujjwal Gupta",
+
+branch: "Mechanical Engineering",
+
+rollNumber: "20226154",
+
+linkedin: "https://www.linkedin.com/in/ujjwal-gupta25",
+
+image: "./images/creators/Ujjwal Gupta.jpg",
+
+
+
+ }
+  
   {
     name: "Sreemoyee Basu",
     branch: "ECE",
